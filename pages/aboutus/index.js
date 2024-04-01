dd249488c1f73f6c4b0d70eeda318668c5e6cdbd
Sonal Agrawal <sonalfeb7@gmail.com>
2024-04-01 18:40:32 +0530

    Final Touch To About Us Page

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,6 +1,13 @@
+import Link from 'next/link'
 const AboutUs = () =>{
     return (
-      <div>About Us Page</div>
+      <div>About Us Page
+        <ul>
+          <li><Link href="/aboutus/1">Yash</Link></li>
+          <li><Link href="/aboutus/2">Vaibhav</Link></li>
+          <li><Link href="/aboutus/3">Suresh</Link></li>
+        </ul>
+      </div>
     )
   }
   export default AboutUs;
